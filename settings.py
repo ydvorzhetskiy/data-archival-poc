6a2a2ca9a50eb581566213c622cc9f445170d781
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-11 22:15:49 +0600

    Set up project

---
+++ settings.py
@@ -0,0 +1,27 @@
+settings = [{
+    'application': 'Ticketing',
+    'dataset_name': 'ticket_as',
+    'table_name': 'ticket_airline',
+    'bq_retention_days': 1110,
+    'gsc_retention_days': 690,
+    'schedule': None,
+    'partition_column': 'eda_load_datetime',
+    'gsc_archival_path': 'gs://archiveddata_ticketing/core/ticketing/ticket_as/processeddata/',
+    'standard': 0,
+    'archival': 690,
+    'near_line': 0,
+    'to_be_archived': 'Y'
+}, {
+    'application': 'Ticketing',
+    'dataset_name': 'ticket_as',
+    'table_name': 'ticket_airline_error',
+    'bq_retention_days': 7,
+    'gsc_retention_days': None,
+    'schedule': None,
+    'partition_column': 'eda_load_datetime',
+    'gsc_archival_path': None,
+    'standard': None,
+    'archival': None,
+    'near_line': None,
+    'to_be_archived': 'N'
+}]
